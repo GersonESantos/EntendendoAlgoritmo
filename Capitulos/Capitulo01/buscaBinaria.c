340ef482a36d7a1402500b1a580277b8805a22d1
GersonESS <gersones@outlook.com>
2024-12-21 19:16:31 -0300

    Update buscaBinaria.c

--- Capitulos/Capitulo01/buscaBinaria.c
+++ Capitulos/Capitulo01/buscaBinaria.c
@@ -1,9 +1,29 @@
 #include <stdio.h>
-#include <stdlib.h>
+int procurabinaria(int[], int, int);
 
 int main() {
-	int myList[] = {0, 1, 2, 3, 5, 7, 9};
+	int minhalista[] = {0, 1, 2, 3, 5, 7, 9};
+	int len = sizeof(minhalista) / sizeof(minhalista[0]);
+    printf("%d\n", procurabinaria(minhalista, 3, len));  // 1
+	printf("%d\n", procurabinaria(minhalista, -1, len)); //-1
 	printf("Vetor \n ");
 	
 	return 0;
+}
+
+int procurabinaria(int lista[], int item, int len) {
+	int baixo = 0;
+	int alto = len;
+	while (baixo <= alto) {
+		int meio = (baixo + alto) / 2;
+		int palpite = lista[meio];
+		if (palpite == item) {
+			return meio;
+		} else if (palpite > item) {
+			alto = meio - 1;
+		} else {
+			baixo = meio + 1;
+		}
+	}
+	return -1; // número não encontrado
 }
